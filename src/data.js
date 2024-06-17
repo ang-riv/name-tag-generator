946e8d061973be96a8ceb8c30112a76e6803791a
a.rivera <a.riveraa99@gmail.com>
2024-06-17 11:47:07 -0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const names = [
+    {
+      id: 1,
+      firstName: "Laios",
+    },
+    {
+      id: 2,
+      firstName: "Chilchuk",
+    },
+    {
+      id: 3,
+      firstName: "Senshi",
+    },
+    {
+      id: 4,
+      firstName: "Marcille",
+    },
+    {
+      id: 5,
+      firstName: "Izutsumi",
+    },
+  ];
+  
+  // each thing in the object is called a PROP (property)
+  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { NameTag } from "./NameTag";
+import { names } from "./data";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// callback fcn
+// mapping over the individual names
+const renderNameTag = (name) => (
+  // use the component + unique id + actual name
+  // for each name in the array, return a NameTag component
+  // that grabs the first name and the id of each
+  <NameTag key={name.id} name={name.firstName} />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// app component
+// needs to be turned into an explicit array as we need to return something
+const App = () => {
+  // the array that we are mapping over
+  // and the variable we are storing the new array from map in
+  const NameTagElements = names.map(renderNameTag);
+  // return the ui elements
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {NameTagElements}
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
